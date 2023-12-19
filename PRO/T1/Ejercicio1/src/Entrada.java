4682a87045399f74386c53f137b4538943b7d0e0
Develop and System <developandsystem@gmail.com>
2023-12-19 10:40:39 +0100

    actualizado desde intellij

--- PRO/T1/Ejercicio1/src/Entrada.java
+++ PRO/T1/Ejercicio1/src/Entrada.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Entrada {
 
     public static void main(String[] args) {
         System.out.println("Esto es un ejemplo de ejecucion");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("indica tu nmbre");
+        String nombre = scanner.next();
+        System.out.println("El nombre introducido es "+nombre);
     }
 }
